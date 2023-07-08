1e04bdfae8ef2e42b321728c9ac6e4df4aee654b
sahil verma <100288753+Romian11@users.noreply.github.com>
2023-07-08 14:59:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const hbs = require("hbs");
+const path = require("path");
+const app = express();
+
+const port = process.env.PORT || 3000;
+const partial_path = path.join(__dirname, "./views/partials");
+app.use(express.static("./public"));
+hbs.registerPartials(partial_path);
+app.set("view engine", hbs);
+app.set("views", "./views");
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index.hbs");
+});
+
+app.get("/coin", (req, res) => {
+  const input = req.query.tvwidgetsymbol;
+  
+  res.render("detailedview.hbs", { src: input });
+
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.hbs");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup.hbs");
+});
+app.get("/forgetpassword", (req, res) => {
+  res.render("signup.hbs");
+});
+app.get("/details", (req, res) => {
+  res.render("start_detail.hbs");
+});
+app.get("/wallet", (req, res) => {
+  res.render("wallet.hbs");
+});
+
+app.listen(port);
